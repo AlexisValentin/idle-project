5ee77909302b15e239bb6ee049c8f29365c15be5
Alexis Valentin <alexis.valentin-ext@mappy.com>
2025-04-14 23:03:05 +0200

    feat: allow factory renaming and factory renaming cancelation

--- src/lib/FactoryName.svelte
+++ src/lib/FactoryName.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { LOCAL_STORAGE_KEYS } from '../constants/storage.constants'
-  import { getFromLocalStorage, saveToLocalStorage } from '../utils/storage'
+  import { userState } from '../store/userState.svelte'
+  import { saveToLocalStorage } from '../utils/storage'
 
-  let factoryName = $state<string | null>(
-    getFromLocalStorage(LOCAL_STORAGE_KEYS.FACORY_NAME),
-  )
+  let isEditingFactoryName = $state(false)
 
   const getNewFactoryName = () => {
     if (!document) return null
@@ -14,6 +13,7 @@
     ) as HTMLInputElement
 
     if (htmlElement?.value) {
+      if (isEditingFactoryName) isEditingFactoryName = false
       saveToLocalStorage(LOCAL_STORAGE_KEYS.FACORY_NAME, htmlElement.value)
 
       return htmlElement.value
@@ -23,12 +23,23 @@
   }
 </script>
 
-{#if factoryName}
-  <h2>Your factory name is {factoryName}</h2>
+{#if userState.factoryName && !isEditingFactoryName}
+  <h2>Your factory name is {userState.factoryName}</h2>
+  <button onclick={() => (isEditingFactoryName = true)}>Edit name</button>
 {:else}
   <form>
-    <label for="factoryName">Please give your factory a name:</label>
-    <input id="factoryName" name="factoryName" type="text" />
-    <button onclick={() => (factoryName = getNewFactoryName())}>OK</button>
+    <label for="factoryName">Please provide a name to your factory:</label>
+    <input
+      id="factoryName"
+      name="factoryName"
+      type="text"
+      placeholder={userState.factoryName ?? ''}
+    />
+    <button onclick={() => (userState.factoryName = getNewFactoryName())}
+      >Validate</button
+    >
+    {#if isEditingFactoryName}
+      <button onclick={() => (isEditingFactoryName = false)}>Cancel</button>
+    {/if}
   </form>
 {/if}
---
+++ src/store/userState.svelte.ts
@@ -0,0 +1,10 @@
+import { LOCAL_STORAGE_KEYS } from '../constants/storage.constants'
+import { getFromLocalStorage } from '../utils/storage'
+
+interface UserState {
+  factoryName: string | null
+}
+
+export const userState = $state<UserState>({
+  factoryName: getFromLocalStorage(LOCAL_STORAGE_KEYS.FACORY_NAME),
+})
